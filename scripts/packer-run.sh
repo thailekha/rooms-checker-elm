8bcce3c6bdbb789bb730ac4f728fc659079ba677
Thai Le <thailekha@yahoo.com>
2017-11-16 01:54:23 +0000

    reecnrypt creds

--- scripts/packer-run.sh
+++ scripts/packer-run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-packer build -var "aws_access_key=$AWS_ACCESS_KEY" -var "aws_secret_key=$AWS_SECRET_KEY" "$(pwd)/scripts/aws-ebs.json"
+PACKER_LOG=1 packer build -var "aws_access_key=$AWS_ACCESS_KEY" -var "aws_secret_key=$AWS_SECRET_KEY" "$(pwd)/scripts/aws-ebs.json"
