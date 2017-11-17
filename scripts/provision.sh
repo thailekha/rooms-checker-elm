b573e1fcc11bbde49a6ef19668b4c618bfeeb9bd
Thai Le <thailekha@yahoo.com>
2017-11-17 00:39:33 +0000

    amazon ami

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-set -ex # print command before executing
-
 echo "--- SHELL PROVISIONING ---"
 
+set -ex # print command before executing
+
 pwd
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
+sudo yum -y install nodejs
 sudo npm install -g n
 sudo n 6.11.2
 mv /tmp/rooms-checker-elm.tgz .
