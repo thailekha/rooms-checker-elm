835ed0a1d7110047abe78206281334108a323910
Thai Le <thailekha@yahoo.com>
2017-11-20 00:53:08 +0000

    refactor and add comments

--- scripts/deploy-aws.tf
+++ scripts/deploy-aws.tf
@@ -9,42 +9,47 @@ provider "aws" {
   region     = "${var.region}"
 }
 
+# location of the remote state
 terraform {
   backend "s3" {
-    bucket = "terraform-state-assessment"
-    key    = "network/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "terraform-state-assessment"
+    key     = "network/terraform.tfstate"
+    region  = "us-east-1"
     encrypt = true
   }
 }
 
+# location of the remote state
 data "terraform_remote_state" "state" {
   backend = "s3"
   # cannot use variables in terraform{backend{}} block so extend s3 here. notice to include everything
   config {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
-    bucket = "terraform-state-assessment"
-    key    = "network/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
+    bucket     = "terraform-state-assessment"
+    key        = "network/terraform.tfstate"
+    region     = "us-east-1"
+    encrypt    = true
   }
 }
 
+# availability_zones, used to configure the load balancer later
 data "aws_availability_zones" "available" {}
 
+# a reference to the default vpc
 resource "aws_default_vpc" "default" {
     tags {
         Name = "Default VPC"
     }
 }
 
+# Security group for the load balancer
 resource "aws_security_group" "elb" {
   name        = "rooms_checker_elb_sg"
   description = "Used for ELB"
   vpc_id      = "${aws_default_vpc.default.id}"
 
-  # HTTP access from anywhere
+  # Inbound rule - HTTP access from anywhere
   ingress {
     from_port   = 80
     to_port     = 80
@@ -52,7 +57,7 @@ resource "aws_security_group" "elb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # outbound internet access
+  # outbound rule
   egress {
     from_port   = 0
     to_port     = 0
@@ -61,20 +66,21 @@ resource "aws_security_group" "elb" {
   }
 }
 
+# Security group for the EC2 instance that will run the app
 resource "aws_security_group" "frontend" {
   name        = "rooms_checker_frontend_sg"
   description = "Used for frontend instance"
   vpc_id      = "${aws_default_vpc.default.id}"
 
-  # HTTP access from anywhere
+  # Inbound rule accepting any TCP traffic from the load balancer
   ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
+    from_port       = 5000
+    to_port         = 5000
+    protocol        = "tcp"
     security_groups = ["${aws_security_group.elb.id}"]
   }
 
-  # outbound internet access
+  # outbound rule to anywhere
   egress {
     from_port   = 0
     to_port     = 0
@@ -83,23 +89,27 @@ resource "aws_security_group" "frontend" {
   }
 }
 
+# The EC2 instance that will run the app, named "frontend"
 resource "aws_instance" "frontend" {
-  ami           = "${var.ami_id}"
-  instance_type = "t2.micro"
-
-  vpc_security_group_ids = ["${aws_security_group.frontend.id}"]
+  ami                     = "${var.ami_id}"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = ["${aws_security_group.frontend.id}"]
 
   lifecycle {
+  # if this isntance needs updating, 
+  # terraform creates a new identical instance, 
+  # apply the updates to the new instance, 
+  # and finally destroy this instance
     create_before_destroy = true
   }
 }
 
+# Load balancer pointing to the "frontend" ec2 instance
 resource "aws_elb" "web" {
-  name = "rooms-checker-elb"
-
-  instances       = ["${aws_instance.frontend.id}"]
-  availability_zones = ["${aws_instance.frontend.availability_zone}"]
-  security_groups = ["${aws_security_group.elb.id}"]  
+  name                = "rooms-checker-elb"
+  instances           = ["${aws_instance.frontend.id}"]
+  availability_zones  = ["${aws_instance.frontend.availability_zone}"]
+  security_groups     = ["${aws_security_group.elb.id}"]
 
   listener {
     instance_port     = 5000
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -6,13 +6,19 @@ set -ex # print command before executing
 APP_PATH=/usr/share/rooms-checker
 pwd
 
+# Install Node.js
 curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 yum -y install nodejs
+
+# Set Node.js version to 6.11.2
 npm install -g n
 n 6.11.2
+
+# Extract the app artifact to appropriate location
 mkdir $APP_PATH
 mv /tmp/rooms-checker-elm.tgz $APP_PATH/.
 cd $APP_PATH && tar -xvzf rooms-checker-elm.tgz
 cd $APP_PATH/package && npm i --production && tar -xvzf dist.tgz
 
+# Configure the OS to run the app at startup
 echo "cd $APP_PATH/package && npm run serve" >> /etc/rc.local
